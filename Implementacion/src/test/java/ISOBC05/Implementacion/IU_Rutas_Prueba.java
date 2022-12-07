d9fb521e73e359a74315583dcbaa43e7a30e15bd
IsaacGonzalez1 <Isaac.Gonzalez1@alu.uclm.es>
2022-12-07 12:32:11 +0100

    Actualización

---
+++ Implementacion/src/test/java/ISOBC05/Implementacion/IU_Rutas_Prueba.java
@@ -0,0 +1,10 @@
+package DClases_General_It1.cliente_Peregrino.Presentación;
+
+public class IU_Rutas {
+
+	public string mostrar_listado_rutas() {
+		// TODO - implement IU_Rutas.mostrar_listado_rutas
+		throw new UnsupportedOperationException();
+	}
+
+}
