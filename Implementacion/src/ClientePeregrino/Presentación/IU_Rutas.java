8204011925c57d850b087d13a5f4f5d40f951dda
IsaacGonzalez1 <Isaac.Gonzalez1@alu.uclm.es>
2022-12-12 20:45:11 +0100

    Implementacion Iteracion 3

--- Implementacion/src/ClientePeregrino/Presentación/IU_Rutas.java
+++ Implementacion/src/ClientePeregrino/Presentación/IU_Rutas.java
@@ -1,9 +1,19 @@
-package DClases_General_It1.cliente_Peregrino.Presentación;
+package DClases_Iteracion1.ClientePeregrino.Presentación;
 
 public class IU_Rutas {
 
 	public string mostrar_listado_rutas() {
 		// TODO - implement IU_Rutas.mostrar_listado_rutas
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrar_lista_negocios() {
+		// TODO - implement IU_Rutas.mostrar_lista_negocios
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrar_info_negocio() {
+		// TODO - implement IU_Rutas.mostrar_info_negocio
 		throw new UnsupportedOperationException();
 	}
 
--- Implementacion/src/Servidor/Dominio/SirvienteServer.java
+++ Implementacion/src/Servidor/Dominio/SirvienteServer.java
@@ -1,9 +1,18 @@
-package DClases_General_It1.Servidor.Dominio;
+package DClases_Iteracion1.Servidor.Dominio;
 
 public class SirvienteServer {
 
 	public Ruta planificacion_ruta() {
 		// TODO - implement SirvienteServer.planificacion_ruta
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param ruta
+	 */
+	public Negocio mostrar_negocios(Ruta ruta) {
+		// TODO - implement SirvienteServer.mostrar_negocios
 		throw new UnsupportedOperationException();
 	}
 
