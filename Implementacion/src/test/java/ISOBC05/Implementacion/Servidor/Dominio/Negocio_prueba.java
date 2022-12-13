d09eaf1f2bc93e5ca49d3f876d21fdb6817f6ae1
EnriqueReyesGonzalez <115165698+EnriqueReyesGonzalez@users.noreply.github.com>
2022-12-13 21:11:38 +0100

    Add files via upload

---
+++ Implementacion/src/test/java/ISOBC05/Implementacion/Servidor/Dominio/Negocio_prueba.java
@@ -0,0 +1,9 @@
+package DClases_Iteracion1.Servidor.Dominio;
+
+public class Negocio {
+
+	private string nombre;
+	private string dirección;
+	private string tipoNegocio;
+
+}
---
+++ Implementacion/src/test/java/ISOBC05/Implementacion/Servidor/Dominio/Servidor_prueba.java
@@ -0,0 +1,7 @@
+package DClases_Iteracion1.Servidor.Dominio;
+
+public class Servidor {
+
+	private string proxy;
+
+}
