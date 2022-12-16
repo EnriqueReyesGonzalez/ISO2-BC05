383afcd2a84457b6494f15dedbfef161a02fb74b
andreslopez150 <114391531+andreslopez150@users.noreply.github.com>
2022-12-16 18:17:47 +0100

    Add files via upload

---
+++ ClientePeregrino/Presentación/IU_Principal.java
@@ -0,0 +1,10 @@
+package DClases_Iteracion1.ClientePeregrino.Presentación;
+
+public class IU_Principal {
+
+	public void elegir_opciones() {
+		// TODO - implement IU_Principal.elegir_opciones
+		throw new UnsupportedOperationException();
+	}
+
+}
