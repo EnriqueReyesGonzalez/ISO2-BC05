90b41b5958cb00fecfcd43217bb910558c806412
andreslopez150 <114391531+andreslopez150@users.noreply.github.com>
2022-12-20 12:52:34 +0100

    Update Negocio.java

--- Implementacion/src/Servidor/Dominio/Negocio.java
+++ Implementacion/src/Servidor/Dominio/Negocio.java
@@ -1,9 +1,9 @@
-package DClases_Iteracion1.Servidor.Dominio;
+package DClases_General_It1.Servidor.Dominio;
 
 public class Negocio {
 
 	private string nombre;
-	private string dirección;
+	private string direcciÃ³n;
 	private string tipoNegocio;
 
 }
